72cac38afe35ab75c8c179166dc1115c08345478
Evandro Neto <evandro.rsneto@gmail.com>
2025-02-25 16:17:09 -0300

    att

--- matriz_dinamica.c
+++ matriz_dinamica.c
@@ -20,35 +20,24 @@ TCelula *alocar_celula() {
     return celula;
 }
 
-void definir_matriz(TCelula **celula, int linha, int coluna) {
-    int i, j;
-    TCelula *linha_atual = NULL, *coluna_atual = NULL;
-
-    if (*celula == NULL) {
-        *celula = alocar_celula();
+void liberar_bloco(TCelula *celula) {
+    if (celula == NULL) {
+        printf("Celula nula\n");
+        return;
     }
-
-    linha_atual = *celula;
-
-    for (i = 0; i < linha; i++) {
-        coluna_atual = linha_atual;
-        for (j = 0; j < coluna - 1; j++) {
-            
-            if (coluna_atual->direita == NULL) {
-                coluna_atual->direita = alocar_celula();
-            }
-            coluna_atual = coluna_atual->direita;
-        
-        }
-
-        if (i < (linha - 1)) {
-            if (linha_atual->abaixo == NULL) {
-                linha_atual->abaixo = alocar_celula();
-            }
-        }
-
-        linha_atual = linha_atual->abaixo;
-    }    
+    printf("Celula não nula\n");
+    if (celula->abaixo != NULL) {
+        printf("Liberando abaixo\n");
+        liberar_bloco(celula->abaixo);
+    }
+    if (celula->direita != NULL) {
+        printf("Liberando direita\n");
+        liberar_bloco(celula->direita);
+        celula->direita = NULL;
+    }
+    
+    free(celula);
+    celula = NULL;
 }
 
 TCelula *get_celula(TCelula *celula, int linha, int coluna) {
@@ -61,6 +50,46 @@ TCelula *get_celula(TCelula *celula, int linha, int coluna) {
     }
     return celula;
 }
+
+void definir_matriz(TCelula **celula, int linha, int coluna) {
+    int i, j;
+    TCelula *linha_atual = NULL, *coluna_atual = NULL, *coluna_acima = NULL;
+
+    if (*celula == NULL) {
+        *celula = alocar_celula();
+    }
+
+    linha_atual = *celula;
+
+    for (i = 0; i < linha; i++) {
+        coluna_atual = linha_atual;
+        for (j = 1; j < coluna ; j++) {
+            
+            if (coluna_atual->direita == NULL) {
+                coluna_atual->direita = alocar_celula();
+            }
+            coluna_atual = coluna_atual->direita;
+
+            if (coluna_acima != NULL) {
+                coluna_acima->abaixo = coluna_atual;
+                coluna_acima = coluna_acima->direita;             
+            }
+
+        }
+        if (i < (linha - 1)) {
+            if (linha_atual->abaixo == NULL) {
+                linha_atual->abaixo = alocar_celula();
+            }
+        }
+        coluna_acima = linha_atual->direita;
+        linha_atual = linha_atual->abaixo;
+    }
+
+    liberar_bloco(get_celula(*celula, 0, coluna));
+    //liberar_bloco(get_celula(*celula, linha, 0));
+}
+
+
 
 void set_celula(TCelula *celula, int linha, int coluna, int valor) {
     TCelula *celula_atual = get_celula(celula, linha, coluna);
@@ -92,9 +121,9 @@ int main() {
 
     printf("\n");
 
-    definir_matriz(&matriz, 4, 4);
-    set_celula(matriz, 3, 3, 4);
+    definir_matriz(&matriz, 2, 2);
     print_matriz(matriz);
+
     return 0;
 }
 
