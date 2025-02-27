1131a1abbe42daf82c25bd11e593bae8754bd684
Evandro Neto <evandro.rsneto@gmail.com>
2025-02-27 12:49:50 -0300

    Add user relationship management and buffer cleaning functions

--- rede_social.c
+++ rede_social.c
@@ -13,6 +13,12 @@ typedef struct tipoCelula {
     struct tipoCelula *direita;
 } TCelula;
 
+
+void limpar_buffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
+
 void set_matriz(TCelula **celula) {
     *celula = NULL;
 }
@@ -103,13 +109,6 @@ void print_matriz(TCelula *celula) {
     }
 }
 
-
-void set_relacionamento(TCelula *celula, int indice_user1, int indice_user2, bool amigos) {
-    set_bool_celula(celula, indice_user1, indice_user2, amigos);
-    set_bool_celula(celula, indice_user2, indice_user1, amigos);
-}
-
-
 int menu(){
 	int opcao;
 	system("CLS");
@@ -117,11 +116,12 @@ int menu(){
 	printf("0 - Sair (Encerrar Aplicacao).\n");
 	printf("1 - Inserir NOVO usuario.\n");
 	printf("2 - Registrar amizade entre usuarios.\n");
-	printf("3 - Indicar novas amizades para usuarios.\n\n");
-	printf("Informe OPCAO desejada: ");
+	printf("3 - Indicar novas amizades para usuarios.\n");
+    printf("4 - Mostrar relacionamentos.\n");
+	printf("\nInforme OPCAO desejada: ");
 	scanf("%d", &opcao);
 	
-	if((opcao > 3) || (opcao < 0)){
+	if((opcao > 4) || (opcao < 0)){
 		printf("\n\n\tERRO:\n\tOPCAO invalida. Tente novamente.\n\n");
 		system("PAUSE");
 	}
@@ -129,21 +129,75 @@ int menu(){
 	return opcao;
 }
 
-
 void inserir_usuario(int *qtd_usuarios, TCelula *usuarios, TCelula  *relacionamentos){
-    char nome[30];
+    str nome;
+
     printf("Digite o nome do usuario: ");
-    scanf("%s", nome);
+
+    limpar_buffer();
+    fgets(nome, sizeof(nome), stdin);
+    nome[strcspn(nome, "\n")] = 0;
 
     (*qtd_usuarios)++;
 
     definir_matriz(&usuarios, *qtd_usuarios, 1);
     definir_matriz(&relacionamentos, *qtd_usuarios, *qtd_usuarios);
-
-    set_str_celula(usuarios, *qtd_usuarios, 0, nome);
+    
+    set_str_celula(usuarios, *qtd_usuarios - 1, 0, nome);
 
     printf("Usuario inserido com sucesso!\n");
+    system("PAUSE");
 }
+
+void print_usuarios(TCelula *celula) {
+    int i = 0;
+    while (celula != NULL) {
+        printf("%d - %s\n", i++, celula->string);
+        celula = celula->abaixo;
+    }
+}
+
+void set_relacionamento(TCelula *relacionametos, int indice_user1, int indice_user2, bool amigos) {
+    set_bool_celula(relacionametos, indice_user1, indice_user2, amigos);
+    set_bool_celula(relacionametos, indice_user2, indice_user1, amigos);
+}
+
+void add_relacionamento(TCelula *usuarios, TCelula *relacionametos) {
+    int indice_user1, indice_user2;
+    print_usuarios(usuarios);
+    printf("Digite os indices dos usuários separados por espaço: ");
+    scanf("%d %d", &indice_user1, &indice_user2);
+    set_relacionamento(relacionametos, indice_user1, indice_user2, true);
+}
+
+
+void print_relacionamentos(TCelula *usuarios, TCelula *relacionamentos) {
+    TCelula *linha_atual = relacionamentos, *coluna_atual, *usuario_atual = usuarios;
+    int biggest_name = 0, name_len;
+
+    // while (usuario_atual != NULL) {
+    //     name_len = strlen(usuario_atual->string);
+    //     if (name_len > biggest_name) {
+    //         biggest_name = name_len;
+    //     }
+    //     usuario_atual = usuario_atual->abaixo;
+    // }
+
+    while (linha_atual != NULL) {
+        coluna_atual = linha_atual;
+
+        printf("%-10s ", usuario_atual->string);
+
+        usuario_atual = usuario_atual->abaixo;
+        while (coluna_atual != NULL) {
+                printf("%d ", coluna_atual->boolean);
+            coluna_atual = coluna_atual->direita;
+        }
+        printf("\n");
+        linha_atual = linha_atual->abaixo;
+    }
+}
+
 
 int main() {
     int opcao, qtd_usuarios = 4;
@@ -167,16 +221,20 @@ int main() {
             break;
         
         case 2:
+            add_relacionamento(usuarios, relacionamentos);
             break;
 
         case 3:
+            break;
+
+        case 4:
+            print_relacionamentos(usuarios, relacionamentos);
+            system("PAUSE");
             break;
         
         default:
             break;
         }
-			
-        printf("gfdgsdfgsdfgdsf");
 	} while(opcao != 0);
 
     return 0;
